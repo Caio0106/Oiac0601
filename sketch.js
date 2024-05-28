8d4f0cdbb0609cd4c39eaed9e9a5feba901c10b9
Caio0106 <00001068690446SP@al.educacao.sp.gov.br>
2024-05-28 09:27:11 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+unction setup() {
+    createCanvas(600, 600);
+    
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  
+  }
